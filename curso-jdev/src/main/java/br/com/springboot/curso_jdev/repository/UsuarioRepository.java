1b28461e63fadc7b857b3ee7464283ed2750915f
Stephanie de França <stephanie.dfranca@gmail.com>
2024-08-14 18:58:51 -0300

    first commit

---
+++ curso-jdev/src/main/java/br/com/springboot/curso_jdev/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package br.com.springboot.curso_jdev.repository;
+
+import br.com.springboot.curso_jdev.model.Usuario;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
+    
+}
